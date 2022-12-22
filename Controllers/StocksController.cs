ffcc4f3f519fe0ff135615142fb022da7203bed4
nguyenviet3057 <nguyenviet3057@gmail.com>
2022-12-22 09:36:59 +0700

    push code

---
+++ Controllers/StocksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookStoreEntity.Models;
+
+namespace BookStoreEntity.Controllers
+{
+    public class StocksController : Controller
+    {
+        private BOOKSTOREEntities db = new BOOKSTOREEntities();
+
+        // GET: Stocks
+        public ActionResult Index()
+        {
+            return View(db.Stocks.ToList());
+        }
+
+        // GET: Stocks/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stocks stocks = db.Stocks.Find(id);
+            if (stocks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stocks);
+        }
+
+        // GET: Stocks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stocks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "STOCK_ID,isbn,Author_name,Title_name,Publisher_name,Published_year,Supplier_ID,Retail_price")] Stocks stocks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stocks.Add(stocks);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(stocks);
+        }
+
+        // GET: Stocks/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stocks stocks = db.Stocks.Find(id);
+            if (stocks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stocks);
+        }
+
+        // POST: Stocks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "STOCK_ID,isbn,Author_name,Title_name,Publisher_name,Published_year,Supplier_ID,Retail_price")] Stocks stocks)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stocks).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(stocks);
+        }
+
+        // GET: Stocks/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Stocks stocks = db.Stocks.Find(id);
+            if (stocks == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stocks);
+        }
+
+        // POST: Stocks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Stocks stocks = db.Stocks.Find(id);
+            db.Stocks.Remove(stocks);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
